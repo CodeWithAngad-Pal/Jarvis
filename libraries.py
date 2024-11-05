05c7ee6a2276215a0be76f815fe14508f735d81a
Angad Pal <Angad4548p@gmail.com>
2024-11-05 23:01:26 +0530

    Create libraries.py

---
+++ libraries.py
@@ -0,0 +1,78 @@
+      #            Core Libraries
+
+import speech_recognition as sr          # for Speech recognition
+import pyttsx3                     # for text-to-speech conversion
+import datetime              # for handling date and time
+import os                  # for operating system functionalities
+import sys               # for system-specific parameters and function
+import random               # for generating random numbers 
+import json              # for handling JSON data
+import requests          # for making HTTP requests
+
+
+      #            Natural language Processing
+
+import nltk                 # for natural language processing
+from nltk.chat.util import Chat , reflectiion       # for simple chatbot functionality
+
+      #            Data Handling
+
+import pandas as pd            # for data manipulation and analysis
+import numpy as np             # for numerical computations
+
+      #            AI and Machine Learning
+
+import torch               # for deep learning (if using Py torch)
+import transformers        #  for NLP models like BERT or GPT
+import tensorflow as tf     #  for deep learning (if using tensorflow)
+
+      #          GUI Development (If application )
+
+from tkinter import *         #  for creating GUI application
+from tkinter import messagebox     #  for showing message
+
+      #          Web SCraping (If appicable)
+
+from bs4 import BeautifulSoup       #  for web scraping
+
+      #          Scheduling Tasks
+
+import schedule        #  for scheduling Tasks
+
+      #          Additional Fuctionalities
+
+import pyjokes        #  for telling jokes
+import psutil       #  for system and process management
+
+      #          Computer Vision
+
+import cv2           # for Display an image , convert to grayscale and Detect Edges
+
+      #           Generating Code
+
+import openai        # generate any code
+
+      #            Video and Animation Creating
+
+from moviepy.editor import VideoFileClip , TextClip ,CompositeVideoClip , AudioFileClip              #  Basic Video Editing , AddingText , Adding Audio, Create a GIF
+import matplotlib.pyplot as plt         #  versatile Plot Type , Customization , Subplots ,Integration , Interactive Plot
+
+      #            3D Modeling 
+
+import bpy          #  3d Modeling , Animation , Rendering , Simulation , Compositing , Scripting
+import vtk           #  graphics , image processing and visulization
+
+        #        Image Creating
+
+from PIL import Image , ImageDraw, ImageFont        #  opening and saving image manipulation , Drawing , Image Conversion
+
+  #======> Pygame :-
+        import pygame
+
+        #         Website sign-in and Sign-Out
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
